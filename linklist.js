56be05e65600d2f3a68eca7f97667604e79e4ef3
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-04-16 06:05:25 +0800

    Set up

---
+++ linklist.js
@@ -0,0 +1,13 @@
+
+
+//represent the full list
+class LinkedList {
+    append(value) {
+        
+    }
+
+}
+//contains the value and address to the next node both are empty at the start 
+class Node {
+
+}
